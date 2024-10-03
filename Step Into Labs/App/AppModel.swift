b898bb1a0ed20baec66ac8dc18a43ab96303d4f2
Joseph Simpson <joe@radicalappdev.com>
2024-10-03 06:56:58 -0400

    template clean up

---
+++ Step Into Labs/App/AppModel.swift
@@ -0,0 +1,15 @@
+//
+//  AppModel.swift
+//  Step Into Labs
+//
+//  Created by Joseph Simpson on 10/3/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+
+}
---
+++ Step Into Labs/App/StepIntoApp.swift
@@ -0,0 +1,22 @@
+//
+//  Step_Into_LabsApp.swift
+//  Step Into Labs
+//
+//  Created by Joseph Simpson on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct StepIntoApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+    }
+}
