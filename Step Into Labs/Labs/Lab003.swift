6a23cfbd33d962e9a60920681553235c8be40a77
Joseph Simpson <joe@radicalappdev.com>
2024-10-03 07:56:25 -0400

    Lab 003

--- Step Into Labs/Labs/Lab003.swift
+++ Step Into Labs/Labs/Lab003.swift
@@ -15,7 +15,30 @@ import RealityKit
 
 struct Lab003: View {
     var body: some View {
-        Text("Hello, World!")
+        RealityView { content, attachments in
+            let model = ModelEntity(
+                mesh: .generateSphere(radius: 0.1),
+                materials: [SimpleMaterial(color: .black, isMetallic: false)])
+            model.position = SIMD3(x: 0.8, y: 1, z: -2)
+            content.add(model)
+
+            if let attachmentEntity = attachments.entity(for: "SphereLabel") {
+                attachmentEntity.position = model.position + [0, 0.16, 0]
+                model.addChild(attachmentEntity)
+                content.add(attachmentEntity)
+            }
+
+        } update: { content, attachments in
+            // ...
+        } attachments: {
+            Attachment(id: "SphereLabel") {
+                Text("An Immersive Space")
+                    .font(.largeTitle)
+                    .padding(18)
+                    .background(.black)
+                    .cornerRadius(12)
+            }
+        }
     }
 }
 
