a94b7f47c4536276c06407cfb27f75e79719b69c
Joseph Simpson <joe@radicalappdev.com>
2024-10-03 06:44:39 -0400

    Initial Commit

---
+++ Step Into Labs/Step_Into_LabsApp.swift
@@ -0,0 +1,33 @@
+//
+//  Step_Into_LabsApp.swift
+//  Step Into Labs
+//
+//  Created by Joseph Simpson on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_Into_LabsApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
