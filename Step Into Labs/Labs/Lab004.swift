ed0e7657ee31fdd8c8a60313db5c73817cf5aede
Joseph Simpson <joe@radicalappdev.com>
2024-10-06 06:49:11 -0400

    Centering the content

--- Step Into Labs/Labs/Lab004.swift
+++ Step Into Labs/Labs/Lab004.swift
@@ -12,25 +12,32 @@
 
 import SwiftUI
 import RealityKit
-
 struct Lab004: View {
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 0) {
-                ForEach(1..<20) { num in
-                    VStack {
-                        GeometryReader { geo in
-                            Text("Number \(num)")
-                                .font(.largeTitle)
-                                .padding()
-                                .background(.red)
-                                .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).minX) / 8), axis: (x: 0, y: 1, z: 0))
-                                .frame(width: 200, height: 200)
-                                .offset(z: 200)
+        GeometryReader { outerGeo in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 0) {
+                    ForEach(1..<20) { num in
+                        VStack {
+                            GeometryReader { geo in
+                                let centerX = outerGeo.size.width / 2
+                                let itemCenterX = geo.frame(in: .global).midX
+                                let rotation = -Double(itemCenterX - centerX) / 8
+
+                                RoundedRectangle(cornerRadius: 24)
+                                    .foregroundStyle(.black)
+                                    .padding()
+                                    .rotation3DEffect(.degrees(rotation), axis: (x: 0, y: 1, z: 0))
+                                    .frame(width: 200, height: 200)
+                                    .offset(z: 40)
+                            }
+                            .frame(width: 200, height: 200)
                         }
-                        .frame(width: 200, height: 200)
                     }
                 }
+                .padding(.horizontal, outerGeo.size.width / 2 - 100)
+                .padding(.vertical, outerGeo.size.height / 2 - 100)
+
             }
         }
     }
