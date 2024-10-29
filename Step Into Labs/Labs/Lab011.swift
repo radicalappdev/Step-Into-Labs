e06c9428b27e677deefe66bb4ad7cd1441462479
Joseph Simpson <joe@radicalappdev.com>
2024-10-29 15:54:46 -0400

    Working on the gesture for Lab 011

--- Step Into Labs/Labs/Lab011.swift
+++ Step Into Labs/Labs/Lab011.swift
@@ -15,6 +15,9 @@ import RealityKit
 import RealityKitContent
 
 struct Lab011: View {
+
+    @State var subjectEntity: Entity? = nil
+
     var body: some View {
         RealityView { content, attachments in
 
@@ -25,6 +28,8 @@ struct Lab011: View {
 
                     let mat = OcclusionMaterial()
                     subject.components[ModelComponent.self]?.materials[0] = mat
+                    subject.isEnabled = false
+                    subjectEntity = subject
                 }
 
             }
@@ -34,7 +39,20 @@ struct Lab011: View {
         } attachments: {
 
         }
+        .gesture(tapExample)
         .gesture(dragGesture)
+    }
+
+    var tapExample: some Gesture {
+        TapGesture()
+            .targetedToAnyEntity() // 3. make sure to use this line to target entities
+            .onEnded { value in
+                print("TAPPED", value.entity.name)
+
+                if(value.entity.name == "Wall_2") {
+                    subjectEntity?.isEnabled = !subjectEntity!.isEnabled
+                }
+            }
     }
 
     var dragGesture: some Gesture {
@@ -42,11 +60,14 @@ struct Lab011: View {
             .targetedToAnyEntity()
             .onChanged { value in
 
-                let newPost = value.convert(value.location3D, from: .local, to: .scene)
+                if(value.entity.name == "Subject") {
 
-                // Clamp the values in both X and Y
-                value.entity.position.x = min(max(newPost.x, -0.8), 0.8)
-                value.entity.position.y = min(max(newPost.y, 0.2), 1.8)
+                    let newPost = value.convert(value.location3D, from: .local, to: .scene)
+
+                    // Clamp the values in both X and Y
+                    value.entity.position.x = min(max(newPost.x, -0.8), 0.8)
+                    value.entity.position.y = min(max(newPost.y, 0.2), 1.8)
+                }
 
             }
             .onEnded { value in
