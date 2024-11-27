f9c3961ca26045e313ec76dcf23d4dbcd13cecc8
Joseph Simpson <joe@radicalappdev.com>
2024-11-27 17:48:10 -0500

    Turn off the gravity and we might have something

--- Step Into Labs/Labs/Lab015.swift
+++ Step Into Labs/Labs/Lab015.swift
@@ -30,40 +30,100 @@ struct Lab015: View {
 
                     let hingeOrientation = simd_quatf(from: [1, 0, 0], to: [0, 0, 1])
 
+                    // Entities
                     let End_1 = jiggle.findEntity(named: "end1")!
                     let Sphere_1 = jiggle.findEntity(named: "ball1")!
+                    let Sphere_2 = jiggle.findEntity(named: "ball2")!
+                    let Sphere_3 = jiggle.findEntity(named: "ball3")!
+                    let End_2 = jiggle.findEntity(named: "end2")!
 
-                    let attachmentPin = End_1.pins.set(
+                    // Pins and Joints
+                    let attachmentPin1 = End_1.pins.set(
                         named: "end_1_hinge",
                         position: .zero,
                         orientation: hingeOrientation
                     )
 
-                    let relativeJointLocation = End_1.position(
+                    let relativeJointLocation1 = End_1.position(
                         relativeTo: Sphere_1
                     )
 
-
-                    let ballPin = Sphere_1.pins.set(
-                        named: "ball_hinge",
-                        position: relativeJointLocation,
+                    let ballPin1 = Sphere_1.pins.set(
+                        named: "ball_1_hinge",
+                        position: relativeJointLocation1,
                         orientation: hingeOrientation
                     )
 
-                    let revoluteJoint = PhysicsRevoluteJoint(pin0: attachmentPin, pin1: ballPin)
+                    let joint1 = PhysicsRevoluteJoint(pin0: attachmentPin1, pin1: ballPin1)
+
+                    // Connect ball1 to ball2
+                    let relativeJointLocation2 = Sphere_1.position(
+                        relativeTo: Sphere_2
+                    )
+
+                    let ballPin2_1 = Sphere_1.pins.set(
+                        named: "ball_1_to_2_hinge",
+                        position: .zero,
+                        orientation: hingeOrientation
+                    )
+
+                    let ballPin2_2 = Sphere_2.pins.set(
+                        named: "ball_2_hinge",
+                        position: relativeJointLocation2,
+                        orientation: hingeOrientation
+                    )
+
+                    let joint2 = PhysicsRevoluteJoint(pin0: ballPin2_1, pin1: ballPin2_2)
+
+                    // Connect ball2 to ball3
+                    let relativeJointLocation3 = Sphere_2.position(
+                        relativeTo: Sphere_3
+                    )
+
+                    let ballPin3_1 = Sphere_2.pins.set(
+                        named: "ball_2_to_3_hinge",
+                        position: .zero,
+                        orientation: hingeOrientation
+                    )
+
+                    let ballPin3_2 = Sphere_3.pins.set(
+                        named: "ball_3_hinge",
+                        position: relativeJointLocation3,
+                        orientation: hingeOrientation
+                    )
+
+                    let joint3 = PhysicsRevoluteJoint(pin0: ballPin3_1, pin1: ballPin3_2)
+
+                    // Connect ball3 to end2
+                    let relativeJointLocation4 = Sphere_3.position(
+                        relativeTo: End_2
+                    )
+
+                    let ballPin4_1 = Sphere_3.pins.set(
+                        named: "ball_3_to_end2_hinge",
+                        position: .zero,
+                        orientation: hingeOrientation
+                    )
+
+                    let attachmentPin2 = End_2.pins.set(
+                        named: "end_2_hinge",
+                        position: relativeJointLocation4,
+                        orientation: hingeOrientation
+                    )
+
+                    let joint4 = PhysicsRevoluteJoint(pin0: ballPin4_1, pin1: attachmentPin2)
+
+                    // Add all joints to the simulation
                     Task {
-
-                        try revoluteJoint.addToSimulation()
+                        try joint1.addToSimulation()
+                        try joint2.addToSimulation()
+                        try joint3.addToSimulation()
+                        try joint4.addToSimulation()
                     }
-
-
-
-
-
-
                 }
             }
         }
+        .modifier(DragGestureImproved())
     }
 }
 
--- Step Into Labs/Utility/Helpers.swift
+++ Step Into Labs/Utility/Helpers.swift
@@ -14,3 +14,40 @@ extension Date {
         self = dateFormatter.date(from: dateString) ?? Date()
     }
 }
+
+/// Adapted from Example Code 010
+// TODO: make a gesture library that I can use in the labs.
+struct DragGestureImproved: ViewModifier {
+
+    @State var isDragging: Bool = false
+    @State var initialPosition: SIMD3<Float> = .zero
+
+    func body(content: Content) -> some View {
+        content
+            .gesture(
+                DragGesture()
+                    .targetedToAnyEntity()
+                    .onChanged { value in
+
+                        // We we start the gesture, cache the entity position
+                        if !isDragging {
+                            isDragging = true
+                            initialPosition = value.entity.position
+                        }
+
+                        // Calculate vector by which to move the entity
+                        let movement = value.convert(value.gestureValue.translation3D, from: .local, to: .scene)
+
+                        // Add the initial position and the movement to get the new position
+                        value.entity.position = initialPosition + movement
+
+                    }
+                    .onEnded { value in
+                        // Clean up when the gesture has ended
+                        isDragging = false
+                        initialPosition = .zero
+                    }
+            )
+
+    }
+}
