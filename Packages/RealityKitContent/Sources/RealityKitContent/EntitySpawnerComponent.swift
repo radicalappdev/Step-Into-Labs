7768d349c6d31edd4bcc74ee328b786bcfeb2f66
Joseph Simpson <joe@radicalappdev.com>
2024-12-08 08:00:43 -0500

    Larger span area

--- Packages/RealityKitContent/Sources/RealityKitContent/EntitySpawnerComponent.swift
+++ Packages/RealityKitContent/Sources/RealityKitContent/EntitySpawnerComponent.swift
@@ -49,22 +49,22 @@ public class EntitySpawnerSystem: System {
             for _ in 1...spawnerComponent.copies {
                 let clone = entity.clone(recursive: true)
                 clone.components.remove(EntitySpawnerComponent.self)
-                
+
                 // Random spherical coordinates
-                let distance = Float.random(in: 1...3)
+                let distance = Float.random(in: 1...12)
                 let theta = Float.random(in: 0...(2 * .pi))
                 // Only use upper hemisphere (0 to π/2)
                 let phi = Float.random(in: 0...(Float.pi / 2))
-                
+
                 // Convert spherical coordinates to Cartesian (unchanged formula)
                 let x = distance * sin(phi) * cos(theta)
                 let y = distance * cos(phi)  // This gives us the height
                 let z = distance * sin(phi) * sin(theta)
                 clone.position = centerPos + SIMD3(x, y, z)
-                
+
                 entity.parent?.addChild(clone)
             }
-            
+
             // Mark as spawned
             spawnerComponent.hasSpawned = true
             entity.components[EntitySpawnerComponent.self] = spawnerComponent
