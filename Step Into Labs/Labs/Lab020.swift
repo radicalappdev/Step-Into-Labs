9c0d2d585abe30e59e812bb4d6d0a46316d04007
Joseph Simpson <joe@radicalappdev.com>
2024-12-19 06:27:54 -0500

    Example to only collide with the subject

--- Step Into Labs/Labs/Lab020.swift
+++ Step Into Labs/Labs/Lab020.swift
@@ -16,18 +16,35 @@ import RealityKitContent
 
 struct Lab020: View {
 
-    @State private var subscription: EventSubscription?
+    @State var subject: Entity?
+    @State var collisionBeganUnfiltered: EventSubscription?
+    @State var collisionBeganSubject: EventSubscription?
 
     var body: some View {
         RealityView { content, attachments in
             if let scene = try? await Entity(named: "Lab020Scene", in: realityKitContentBundle) {
-                print("scene loaded")
-
                 content.add(scene)
+                subject = scene.findEntity(named: "StepSphereBlue")
 
-                subscription = content.subscribe(to: CollisionEvents.Began.self)  { collisionEvent in
-                    print("Collision between \(collisionEvent.entityA.name) and \(collisionEvent.entityB.name)")
+                // This unfiltered event will fire twice. Once for each entity in the collision
+                // Each will be entityA in their own version of the event, so we don't need the burse for entityB
+                collisionBeganUnfiltered = content.subscribe(to: CollisionEvents.Began.self)  { collisionEvent in
+                    print("Collision unfiltered \(collisionEvent.entityA.name) and \(collisionEvent.entityB.name)")
+                    collisionEvent.entityA.components[ParticleEmitterComponent.self]?.burst()
+
                 }
+
+                collisionBeganSubject = content
+                    .subscribe(to: CollisionEvents.Began.self, on: subject)  { collisionEvent in
+                        print("Collision Subject \(collisionEvent.entityA.name) and \(collisionEvent.entityB.name)")
+                        if let subject {
+                            popupEntity(subject)
+//                            if subject.position.y > 1.025 {
+//                            } else {
+//
+//                            }
+                        }
+                    }
             }
 
         } update: { content, attachments in
@@ -39,6 +56,36 @@ struct Lab020: View {
         }
         .modifier(DragGestureImproved())
     }
+
+    func popupEntity(_ entity: Entity) {
+        let transform = Transform(
+            scale: SIMD3<Float>(repeating: 1),
+            rotation: simd_quatf(angle: 0, axis: SIMD3<Float>(0, 0, 0)),
+            translation: SIMD3<Float>(entity.position.x, entity.position.y + 0.05, entity.position.z)
+        )
+
+        let transform2 = Transform(
+            scale: SIMD3<Float>(repeating: 1),
+            rotation: simd_quatf(angle: 0, axis: SIMD3<Float>(0, 0, 0)),
+            translation: SIMD3<Float>(entity.position.x, 1.0, entity.position.z)
+        )
+
+        entity
+            .move(
+                to: transform,
+                relativeTo: entity.parent!,
+                duration: 0.5,
+                timingFunction: .easeInOut
+            )
+        entity
+            .move(
+                to: transform2,
+                relativeTo: entity.parent!,
+                duration: 1.0,
+                timingFunction: .easeInOut
+            )
+    }
+
 }
 
 #Preview {
