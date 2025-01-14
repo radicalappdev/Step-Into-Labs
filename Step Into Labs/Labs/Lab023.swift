43a3f7861739f1d164b00eba916fa05dcdd834ea
Joseph Simpson <joe@radicalappdev.com>
2025-01-14 16:36:27 -0500

    Working on game state

--- Step Into Labs/Labs/Lab023.swift
+++ Step Into Labs/Labs/Lab023.swift
@@ -15,11 +15,17 @@ import RealityKit
 import RealityKitContent
 
 struct Lab023: View {
-    @State var resetGame = false
+
+    @State var session: SpatialTrackingSession?
+    @State var handControl: Entity?
+
+
+    @State var gameActive = false
     @State var score: Int = 0
 
     @State var collisionBeganSubject: EventSubscription?
 
+    @State var chamber = Entity()
     @State var ball = Entity()
     @State var box = Entity()
 
@@ -27,12 +33,18 @@ struct Lab023: View {
     var body: some View {
         RealityView { content, attachments in
 
+            let configuration = SpatialTrackingSession.Configuration(
+                tracking: [.hand])
+            let session = SpatialTrackingSession()
+            await session.run(configuration)
+
             if let scene = try? await Entity(named: "PhysicsPlayground", in: realityKitContentBundle) {
                 content.add(scene)
 
                 guard let chamber = scene.findEntity(named: "Chamber")  else { return }
                 chamber.setPosition([0, 1.4, -2], relativeTo: nil)
                 chamber.setScale([0.5, 0.5, 0.5], relativeTo: nil)
+                self.chamber = chamber
 
                 if let ball = scene.findEntity(named: "Ball"), let box = scene.findEntity(named: "Box") {
                     self.ball = ball
@@ -40,13 +52,20 @@ struct Lab023: View {
                     collisionBeganSubject = content.subscribe(to: CollisionEvents.Began.self, on: ball)  { collisionEvent in
 
                         if(collisionEvent.entityB == box) {
+                            print("subject collision \(collisionEvent.entityB)")
                             gameOver()
                         }
-                        score += 1
-                        // print("subject collision \(collisionEvent.entityB)")
+                        
+                        if(gameActive) {
+                            score += 1
+                        }
                     }
                 }
-                
+
+                if let handControl = scene.findEntity(named: "HandControl") {
+                    self.handControl = handControl
+                }
+
                 
                 // add the menu
                 if let gameMenu = attachments.entity(for: "GameMenu") {
@@ -66,8 +85,10 @@ struct Lab023: View {
                     }, label: {
                         Text("Start Game")
                     })
-                    if(resetGame) {
+                    if(gameActive == false && score > 0) {
                         Text("You Won in \(score) bounces!")
+                    } else {
+                        Text("Tap to start")
                     }
                 }
                 .padding(10)
@@ -75,10 +96,28 @@ struct Lab023: View {
                 .clipShape(.capsule)
             }
         }
+        .persistentSystemOverlays(.hidden)
+        .upperLimbVisibility(.hidden)
+        .task {
+            while true {
+                // Periodically check the anchor transform and stash it in SwiftUi state.
+
+                if gameActive {
+                    if let anchor = handControl {
+                        let transform = Transform(matrix: anchor.transformMatrix(relativeTo: nil))
+                        chamber.setOrientation(transform.rotation, relativeTo: nil)
+
+                    }
+
+                }
+                try? await Task.sleep(for: .seconds(1/30))
+            }
+
+        }
     }
 
     func startGame() {
-        resetGame = false
+        gameActive = true
         score = 0
         ball.setPosition([0, 0.5, 0], relativeTo: ball.parent)
         if var ballMotion = ball.components[PhysicsMotionComponent.self] {
@@ -92,16 +131,19 @@ struct Lab023: View {
             ball.components.set(ballPhyics)
         }
         box.setPosition([Float.random(in: -0.9...0.9), Float.random(in: -0.9...0.9), Float.random(in: -0.9...0.9)], relativeTo: box.parent)
+        box.isEnabled = true
     }
 
     func gameOver() {
-        resetGame = true
+        gameActive = false
+
         ball.setPosition([0.25, 0.5, 0], relativeTo: ball.parent)
         if var ballPhyics = ball.components[PhysicsBodyComponent.self] {
             ballPhyics.isAffectedByGravity = false
             ball.components.set(ballPhyics)
         }
         box.setPosition([0.25, 0.5, 0], relativeTo: box.parent)
+        box.isEnabled = false
     }
 }
 
