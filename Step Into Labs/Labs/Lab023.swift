923f016334aad94446d15fcf50768f5074335dd8
Joseph Simpson <joe@radicalappdev.com>
2025-01-14 15:07:43 -0500

    adding an attachment

--- Step Into Labs/Labs/Lab023.swift
+++ Step Into Labs/Labs/Lab023.swift
@@ -17,8 +17,13 @@ import RealityKitContent
 struct Lab023: View {
     var body: some View {
 
-        @State var collisionBeganSubject: EventSubscription?
+        @State var resetGame = false
         @State var score: Int = 0
+
+        @State var collisionBeganSubject: EventSubscription?
+
+
+
 
 
         RealityView { content, attachments in
@@ -26,26 +31,51 @@ struct Lab023: View {
             if let scene = try? await Entity(named: "PhysicsPlayground", in: realityKitContentBundle) {
                 content.add(scene)
 
-                if let chamber = scene.findEntity(named: "Chamber") {
-                    chamber.setPosition([0, 1.4, -2], relativeTo: nil)
-                }
+                guard let chamber = scene.findEntity(named: "Chamber")  else { return }
+                chamber.setPosition([0, 1.4, -2], relativeTo: nil)
 
                 if let subject = scene.findEntity(named: "Ball") {
                     print("subject found")
-
                     collisionBeganSubject = content.subscribe(to: CollisionEvents.Began.self, on: subject)  { collisionEvent in
                         score += 1
-                        print("subject collision \(collisionEvent.entityB)")
+//                        print("subject collision \(collisionEvent.entityB)")
                     }
                 }
+                
+                
+                // add the menu
+                if let gameMenu = attachments.entity(for: "GameMenu") {
+                    gameMenu.setPosition([0, -0.75, 1.1], relativeTo: chamber)
+                    gameMenu.scale = [2,2,2]
+                    content.add(gameMenu)
+                }
+
 
             }
 
         } update: { content, attachments in
 
+            if resetGame {
+                print("game reset")
+                if let subject = content.entities.first?.findEntity(named: "Ball") {
+                    subject.setPosition([0, 0.75, 0], relativeTo: subject.parent)
+                    resetGame = false
+                }
+            }
+
         } attachments: {
-            Attachment(id: "AttachmentContent") {
-                Text("")
+            Attachment(id: "GameMenu") {
+                VStack {
+                    Button(action: {
+                        resetGame = true
+                        print("game reset button pressed")
+                    }, label: {
+                        Text("Restart")
+                    })
+                }
+                .padding(10)
+                .background(Color.black)
+                .clipShape(.capsule)
             }
         }
     }
