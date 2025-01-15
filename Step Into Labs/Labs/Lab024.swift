12707057e7fc29fdf6cb538c41d1e0b26b984c42
Joseph Simpson <joe@radicalappdev.com>
2025-01-15 05:57:17 -0500

    fake window background

--- Step Into Labs/Labs/Lab024.swift
+++ Step Into Labs/Labs/Lab024.swift
@@ -18,16 +18,35 @@ struct Lab024: View {
     var body: some View {
         RealityView { content, attachments in
 
-            if let list = attachments.entity(for: "AttachmentContent") {
+            let window = Entity()
+            window.setPosition([1.1, 1.5, -2], relativeTo: nil)
+            content.add(window)
 
+            // Reference view
+            if let list = attachments.entity(for: "AttachmentContent") {
                 list.setPosition([0.75, 1.5, -2], relativeTo: nil)
                 content.add(list)
+            }
 
+            // Building the fake window
+            if let windowBackground = attachments.entity(for: "WindowBackground") {
+                window.addChild(windowBackground)
+                windowBackground.setPosition([0 , 0, 0], relativeTo: window)
             }
 
         } update: { content, attachments in
 
         } attachments: {
+
+            Attachment(id: "WindowBackground") {
+                VStack {
+                    EmptyView()
+                }
+                .frame(width: 400, height: 300)
+                .glassBackgroundEffect()
+            }
+
+
             Attachment(id: "AttachmentContent") {
                 NavigationStack {
                     List {
