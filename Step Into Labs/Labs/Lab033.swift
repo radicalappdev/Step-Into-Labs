9fe8b8e9306c8a67caf7e7c1b20cf89ea4e31738
Joseph Simpson <joe@radicalappdev.com>
2025-02-10 13:35:46 -0500

    This works, but could be improved

--- Step Into Labs/Labs/Lab033.swift
+++ Step Into Labs/Labs/Lab033.swift
@@ -15,22 +15,54 @@ import RealityKit
 import RealityKitContent
 
 struct Lab033: View {
+
+    @State var sceneContent: Entity?
+
+    var teleportTap: some Gesture {
+        SpatialTapGesture()
+            .targetedToAnyEntity()
+            .onEnded { value in
+
+                print("TELEPORTING TO: \(value.entity.name)" )
+
+                guard let sceneContent = self.sceneContent else { return }
+
+                // Calculate the vector from the origin to the tapped position
+                let vectorToTap = value.convert(value.location3D, from: .local, to: sceneContent)
+
+                // Normalize the vector to get a direction from the origin to the tapped position
+                let direction = normalize(vectorToTap)
+
+                // Calculate the distance (or magnitude) between the origin and the tapped position
+                let distance = length(vectorToTap)
+
+                // Calculate the new position by inverting the direction multiplied by the distance
+                let newPosition = -direction * distance
+
+                // Update sceneOffset's X and Z components, leave Y as it is
+                let teleportPosition: SIMD3<Float> = [newPosition.x, 0, newPosition.z]
+
+                print("TELEPORTING POS: \(teleportPosition)" )
+                sceneContent.position = teleportPosition
+
+
+            }
+    }
+
     var body: some View {
-        RealityView { content, attachments in
+        RealityView { content in
 
             if let scene = try? await Entity(named: "TeleportLaps", in: realityKitContentBundle) {
                 content.add(scene)
 
-
+                // Get the scene content and stash it in state
+                if let sceneContent = scene.findEntity(named: "SceneContent") {
+                    self.sceneContent = sceneContent
+                }
             }
 
-        } update: { content, attachments in
-
-        } attachments: {
-            Attachment(id: "AttachmentContent") {
-                Text("")
-            }
         }
+        .gesture(teleportTap)
     }
 }
 
