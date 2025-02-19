0ba35c7feac939f8dc2e8b3a2390e198b6fda71a
Joseph Simpson <joe@radicalappdev.com>
2025-02-19 09:23:12 -0500

    Tap to teleport to an entity

--- Step Into Labs/Labs/Lab034.swift
+++ Step Into Labs/Labs/Lab034.swift
@@ -16,6 +16,11 @@ import RealityKitContent
 
 struct Lab034: View {
 
+    init() {
+        // Make sure to register the component!
+        TeleportTargetComponent.registerComponent()
+    }
+
     @State var sceneContent: Entity?
 
     var body: some View {
@@ -33,6 +38,34 @@ struct Lab034: View {
             }
 
         }
+        .gesture(tapExample)
+
+    }
+
+    var tapExample: some Gesture {
+        SpatialTapGesture()
+            .targetedToEntity(where: .has(TeleportTargetComponent.self))
+            .onEnded { value in
+
+                guard let sceneContent = self.sceneContent else { return }
+
+                // Calculate the vector from the origin to the tapped position
+                let vectorToTap = value.entity.position
+
+                // Normalize the vector to get a direction from the origin to the tapped position
+                let direction = normalize(vectorToTap)
+
+                // Calculate the distance (or magnitude) between the origin and the tapped position
+                let distance = length(vectorToTap)
+
+                // Calculate the new position by inverting the direction multiplied by the distance
+                let newPosition = -direction * distance
+
+                // Update sceneOffset's X and Z components, leave Y as it is
+                sceneContent.position.x = newPosition.x
+                sceneContent.position.z = newPosition.z
+
+            }
     }
 }
 
