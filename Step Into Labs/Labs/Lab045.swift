f1e61fbca9f800abfb4e4d68c8ea4d3389069439
Joseph Simpson <joe@radicalappdev.com>
2025-04-06 08:45:24 -0400

    Basic tap to spin

--- Step Into Labs/Labs/Lab045.swift
+++ Step Into Labs/Labs/Lab045.swift
@@ -16,20 +16,48 @@ import RealityKitContent
 
 struct Lab045: View {
     var body: some View {
-        RealityView { content, attachments in
+        RealityView { content in
+
+            // Create the subject
             let subject = ModelEntity(
                 mesh: .generateBox(size: 0.2),
                 materials: [SimpleMaterial(color: .stepRed, isMetallic: false)]
             )
+
+            // Set up components
+            let collision = CollisionComponent(shapes: [.generateBox(size: .init(repeating: 0.2))])
+            let input = InputTargetComponent()
+            let hover = HoverEffectComponent()
+            subject.components.set([collision, input, hover])
+
+            // Add the subject to the scene
             content.add(subject)
-
-        } update: { content, attachments in
-
-        } attachments: {
-            Attachment(id: "AttachmentContent") {
-                Text("")
-            }
         }
+        .gesture(tap)
+
+    }
+
+    var tap: some Gesture {
+        TapGesture()
+            .targetedToAnyEntity()
+            .onEnded { value in
+                Task {
+
+                    let spinAction = SpinAction(revolutions: 1,
+                                                localAxis: [0, 1, 0],
+                                                timingFunction: .easeInOut,
+                                                isAdditive: false)
+
+                    let spinAnimation = try AnimationResource
+                        .makeActionAnimation(for: spinAction,
+                                             duration: 1.0,
+                                             bindTarget: .transform)
+
+
+                    value.entity.playAnimation(spinAnimation)
+                }
+
+            }
     }
 }
 
