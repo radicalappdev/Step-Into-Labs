be61c6b8dab35e71dc88bdfeee40e91ba5019796
Joseph Simpson <joe@radicalappdev.com>
2025-04-06 09:07:43 -0400

    Cleaning up entity code

--- Step Into Labs/Labs/Lab045.swift
+++ Step Into Labs/Labs/Lab045.swift
@@ -15,50 +15,58 @@ import RealityKit
 import RealityKitContent
 
 struct Lab045: View {
+
+    // Create the subject
+    @State var subject: Entity = {
+        let entity = ModelEntity(
+            mesh: .generateBox(size: 0.2),
+            materials: [SimpleMaterial(color: .stepRed, isMetallic: false)]
+        )
+        let collision = CollisionComponent(shapes: [.generateBox(size: .init(repeating: 0.2))])
+        let input = InputTargetComponent()
+        let hover = HoverEffectComponent()
+        entity.components.set([collision, input, hover])
+        return entity
+    }()
+
     var body: some View {
         RealityView { content in
-
-            // Create the subject
-            let subject = ModelEntity(
-                mesh: .generateBox(size: 0.2),
-                materials: [SimpleMaterial(color: .stepRed, isMetallic: false)]
-            )
-
-            // Set up components
-            let collision = CollisionComponent(shapes: [.generateBox(size: .init(repeating: 0.2))])
-            let input = InputTargetComponent()
-            let hover = HoverEffectComponent()
-            subject.components.set([collision, input, hover])
 
             // Add the subject to the scene
             content.add(subject)
         }
-        .gesture(tap)
+
+        // Create a toolbar with a button to perform the action
+        .toolbar {
+            ToolbarItem(placement: .bottomOrnament, content: {
+                Button(action: {
+                    spinSubject()
+                }, label: {
+                    Text("Spin")
+                })
+            })
+        }
 
     }
 
-    var tap: some Gesture {
-        TapGesture()
-            .targetedToAnyEntity()
-            .onEnded { value in
-                Task {
+    func spinSubject() {
+        Task {
+            // Create the action
+            let spinAction = SpinAction(revolutions: 1,
+                                        localAxis: [0, 1, 0],
+                                        timingFunction: .easeInOut,
+                                        isAdditive: false)
 
-                    let spinAction = SpinAction(revolutions: 1,
-                                                localAxis: [0, 1, 0],
-                                                timingFunction: .easeInOut,
-                                                isAdditive: false)
+            // Use the action in an animation
+            let spinAnimation = try AnimationResource.makeActionAnimation(for: spinAction,
+                                                                          duration: 1,
+                                                                          bindTarget: .transform)
 
-                    let spinAnimation = try AnimationResource
-                        .makeActionAnimation(for: spinAction,
-                                             duration: 1.0,
-                                             bindTarget: .transform)
-
-
-                    value.entity.playAnimation(spinAnimation)
-                }
-
-            }
+            // Play the animation
+            subject.playAnimation(spinAnimation)
+        }
     }
+
 }
 
 #Preview {
