360adcf4fef1fbe28ba7f0479bb46c7c683218b1
Joseph Simpson <joe@radicalappdev.com>
2025-04-30 16:44:46 -0400

    Silly but it works

--- Step Into Labs/Labs/Lab048.swift
+++ Step Into Labs/Labs/Lab048.swift
@@ -10,56 +10,89 @@
 //
 //  Created by Joseph Simpson on 4/30/25.
 
-import SwiftUI
 import RealityKit
 import RealityKitContent
+import SwiftUI
 
 struct Lab048: View {
 
-    // The root for our scene *outside* of the portal
-    private let rootEntity = Entity()
+    @State var windowIsOpen = false
 
-    // An entity that will render the portal
-    private let portalPlane = ModelEntity(
-        mesh: .generatePlane(width: 1.0, height: 1.0),
-        materials: [PortalMaterial()]
-    )
+  // The root for our scene *outside* of the portal
+  private let rootEntity = Entity()
 
-    var body: some View {
-        ZStack {
-            GeometryReader3D { geometry in
-                RealityView { content in
+  // An entity that will render the portal
+  private let portalPlane = ModelEntity(
+    mesh: .generatePlane(width: 1.0, height: 1.0),
+    materials: [PortalMaterial()]
+  )
 
-                    // The root for the content that will appear *inside* the portal
-                    let portalContentRoot = Entity()
-                    portalContentRoot.scale *= 0.5
-                    portalContentRoot.position.y -= 0.5
-                    portalContentRoot.components.set(WorldComponent())
+  var body: some View {
+    GeometryReader3D { geometry in
+      ZStack {
+        RealityView { content in
 
-                    // We'll load some content to add to the portalContentRoot
-                    guard let scene = try? await Entity(named: "PortalSwapRed", in: realityKitContentBundle) else { return }
-                    portalContentRoot.addChild(scene)
-                    portalPlane.components.set(PortalComponent(target: portalContentRoot))
+          // The root for the content that will appear *inside* the portal
+          let portalContentRoot = Entity()
+          portalContentRoot.scale *= 0.5
+          portalContentRoot.position.y -= 0.5
+          portalContentRoot.components.set(WorldComponent())
 
-                    rootEntity.addChild(portalContentRoot)
-                    rootEntity.addChild(portalPlane)
-                    content.add(rootEntity)
-                } update: { content in
-                    // Resize the scene based on the size of the reality view content.
-                    // Based on the example from Apple https://developer.apple.com/documentation/visionos/displaying-a-3d-environment-through-a-portal
-                    let size = content.convert(geometry.size, from: .local, to: .scene)
+          // We'll load some content to add to the portalContentRoot
+          guard let scene = try? await Entity(named: "PortalSwapRed", in: realityKitContentBundle)
+          else { return }
+          portalContentRoot.addChild(scene)
+          portalPlane.components.set(PortalComponent(target: portalContentRoot))
 
-                    portalPlane.model?.mesh = .generatePlane(width: size.x, height: size.y, cornerRadius: 0.03)
+          rootEntity.addChild(portalContentRoot)
+          rootEntity.addChild(portalPlane)
+          content.add(rootEntity)
+        } update: { content in
+          // Resize the scene based on the size of the reality view content.
+          // Based on the example from Apple https://developer.apple.com/documentation/visionos/displaying-a-3d-environment-through-a-portal
+          let size = content.convert(geometry.size, from: .local, to: .scene)
 
-                }
-                .frame(width: geometry.size.width, height: geometry.size.height)
-                .frame(depth: 0.4)
-            }
+          portalPlane.model?.mesh = .generatePlane(
+            width: size.x, height: size.y, cornerRadius: 0.03)
+
         }
+        .opacity(windowIsOpen ? 1.0 : 0.0)
+        .frame(depth: 0.4)
+
+        VStack {
+          HStack {
+            Text("Fruits")
+              .font(.title)
+              .frame(height: 60)
+            Spacer()
+            Button(
+              action: {
+                print("button pressed")
+                  withAnimation(.easeInOut(duration: 0.3)) {
+                      windowIsOpen.toggle()
+                  }
+              },
+              label: {
+                Text("Open Window")
+              })
+          }
+          .padding(.horizontal, 24)
+          .padding(.top, 12)
+          List {
+            Text("Apple")
+            Text("Banana")
+            Text("Orange")
+          }
+        }
+        .glassBackgroundEffect()
+        .offset(y: windowIsOpen ? -360 : 0)
+      }
+      .frame(width: geometry.size.width, height: geometry.size.height)
     }
+  }
 
 }
 
 #Preview {
-    Lab048()
+  Lab048()
 }
