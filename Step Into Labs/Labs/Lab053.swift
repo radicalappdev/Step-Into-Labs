0811552e396dcf4d278829f98ba12234e317591e
Joseph Simpson <joe@radicalappdev.com>
2025-05-22 07:12:18 -0400

    Adding a portal scene with some attachments

--- Step Into Labs/Labs/Lab053.swift
+++ Step Into Labs/Labs/Lab053.swift
@@ -42,15 +42,39 @@ struct Lab053: View {
 
 
             // 4. We'll load some content to add to the portalContentRoot
-//            guard let scene = try? await Entity(named: "TeleportLabs", in: realityKitContentBundle) else { return }
+            guard let scene = try? await Entity(named: "NoFloor", in: realityKitContentBundle) else { return }
 
-            // Let's try loading as an empty scene
-            let scene = Entity()
+            //  add an attachment as a child of an entity
+            if let wetFloorSign = scene.findEntity(named: "wet_floor_sign"),
+               let wetFloorAttachment = attachments.entity(for: "wet_floor_attachment") {
+                // The wet_floor_sign asset was converted from another format. It was scaled to 0.01 on all axes to fit in this scene.
+                // We'll have to scale the attachment to compensate for the scale of the entity
 
-            if let panel = attachments.entity(for: "AttachmentContent") {
-                scene.addChild(panel)
-                panel.position.y = 1.4
-                panel.position.z = -0.4
+                // Add the wetFloorAttachment attachment as a child of the wet floor sign
+                wetFloorSign.addChild(wetFloorAttachment)
+
+                // Adjust the transform to position it just in front of the sign
+                let transform = Transform(scale: .init(repeating: 200), rotation: simd_quatf(Rotation3D(angle: Angle2D(degrees: 11), axis: RotationAxis3D(x: -1, y: 0, z: 0))), translation: [0, 30, 6.7])
+                wetFloorAttachment.transform = transform
+            }
+
+            // use an entity to position the attachment. Add the attachment to the scene content
+            if let trafficCone = scene.findEntity(named: "traffic_cone_02"),
+               let traffiConeAttachment = attachments.entity(for: "traffic_cone_attachment") {
+
+                // For this example, we'll add the attachment directly to the scenc content
+                scene.addChild(traffiConeAttachment)
+
+                // Then we'll use the data from the traffic cone entity to determine the transform for the attachment
+                let transform = Transform(
+                    scale: .init(repeating: 1.0),
+                    rotation: simd_quatf(
+                        Rotation3D(angle: Angle2D(degrees: -24), axis: RotationAxis3D(x: 0, y: 1, z: 0))
+                    ),
+                    translation: trafficCone.position + [0, 0.8 , 0]
+                )
+
+                traffiConeAttachment.transform = transform
             }
 
             portalContentRoot.addChild(scene)
@@ -59,19 +83,52 @@ struct Lab053: View {
         } update: { content, attachments in
 
         } attachments: {
-            Attachment(id: "AttachmentContent") {
-                VStack {
-                    Text("Testing Attachment in Portals")
-                        .font(.title)
-                    Button(action: {
-                        print("testing")
-                    }, label: {
-                        Label("Button", image: "eject.fill")
-                    })
+
+
+            Attachment(id: "wet_floor_attachment") {
+                VStack(spacing: 24) {
+                    Text("CAUTION")
+                        .font(.largeTitle)
+                    ZStack {
+                        Image(systemName: "triangle")
+                            .font(.system(size: 96, weight: .semibold))
+                        Image(systemName: "figure.fall")
+                            .font(.system(size: 42, weight: .heavy))
+                            .offset(y:12)
+                    }
+                    Text("No Floor")
+                        .font(.largeTitle)
                 }
-                    .padding(12)
-                    .glassBackgroundEffect()
+                .foregroundStyle(.black)
+                .textCase(.uppercase)
+                .padding()
             }
+
+            Attachment(id: "traffic_cone_attachment") {
+                VStack(spacing: 24) {
+                    Text("Attachments Render")
+                        .font(.extraLargeTitle)
+                    ZStack {
+                        Image(systemName: "triangle")
+                            .font(.system(size: 96, weight: .semibold))
+                        Image(systemName: "eyes")
+                            .font(.system(size: 36, weight: .heavy))
+                            .offset(y:12)
+                    }
+                    .onTapGesture {
+                        print("does this do anything?")
+                    }
+                    .hoverEffect()
+                    Text("but are not interactive")
+                        .font(.extraLargeTitle)
+                }
+                .padding(24)
+                .foregroundStyle(.white)
+                .textCase(.uppercase)
+                .background(.trafficOrange)
+                .clipShape(.rect(cornerRadius: 24.0))
+            }
+
         }
     }
 }
