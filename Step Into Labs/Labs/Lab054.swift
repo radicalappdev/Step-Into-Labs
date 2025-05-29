49b283a3e258e267b1edb3eab24ecc737f3d167e
Joseph Simpson <joe@radicalappdev.com>
2025-05-29 06:34:23 -0400

    Adding a grid in the background

--- Step Into Labs/Labs/Lab054.swift
+++ Step Into Labs/Labs/Lab054.swift
@@ -18,34 +18,48 @@ struct Lab054: View {
     @Namespace private var hoverNamespace
 
     var body: some View {
-        VStack {
-            Button(action: {
-                print("")
-            }, label: {
-                HStack {
-                    Text("Hover Me")
-                    
+        ZStack {
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 4), count: 12), spacing: 4) {
+                ForEach(0..<96, id: \.self) { _ in
+                    Shrug(hoverNamespace: hoverNamespace)
                 }
-            })
-            .hoverEffect(in: HoverEffectGroup(hoverNamespace)) { effect, isActive, proxy in
-                effect.animation(.easeOut) {
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
 
-                    $0.rotationEffect(.degrees(isActive ? -15 : 0), anchor: .bottomTrailing)
-                        .scaleEffect(isActive ? 2 : 1)
+            VStack {
+                Button(action: {
+                    print("")
+                }, label: {
+                    HStack {
+                        Text("What tilt hover effect?")
+                    }
+                })
+                .glassBackgroundEffect()
+                .hoverEffect(in: HoverEffectGroup(hoverNamespace)) { effect, isActive, proxy in
+                    effect.animation(.easeOut) {
+                        $0.rotationEffect(.degrees(isActive ? -15 : 0), anchor: .center)
+
+                    }
                 }
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .glassBackgroundEffect()
-        .hoverEffect(in: HoverEffectGroup(hoverNamespace)) { effect, isActive, _ in
-            effect.animation(.easeOut) {
-                $0.rotationEffect(.degrees(isActive ? 15 : 0), anchor: .center)
-                .scaleEffect(isActive ? 0.5 : 1)
-            }
-        }
     }
 }
 
+
+fileprivate struct Shrug: View {
+    @State var hoverNamespace: Namespace.ID
+
+    var body: some View {
+        Text("🤷🏻‍♂️")
+            .font(.extraLargeTitle2)
+            .hoverEffect(in: HoverEffectGroup(hoverNamespace)) { effect, isActive, proxy in
+                effect.opacity(isActive ? 0.3 : 0)
+            }
+    }
+}
 
 
 
