acc831e1b658e3f0ba5a421e11f3510d48086183
Joseph Simpson <joe@radicalappdev.com>
2025-06-07 12:01:52 -0400

    This kind of works

--- Step Into Labs/Labs/Lab059.swift
+++ Step Into Labs/Labs/Lab059.swift
@@ -16,43 +16,38 @@ import RealityKitContent
 
 struct Lab059: View {
     var body: some View {
-        RealityView { content, attachments in
-
+        RealityView { content in
             var options = MeshResource.ShapeExtrusionOptions()
-            options.extrusionMethod = .linear(depth: 0.25)
+            options.extrusionMethod = .linear(depth: 0.5)
 
 
-            guard let mesh = try? await MeshResource(extruding: simplePath(in: CGRect()), extrusionOptions: options) else { return }
+            guard let mesh = try? await MeshResource(extruding: simplePath(), extrusionOptions: options) else { return }
 
-            let material = SimpleMaterial(color: .red, isMetallic: false)
+            let material = SimpleMaterial(color: .stepGreen, isMetallic: false)
             let subject = Entity()
             let meshComponent = ModelEntity(mesh: mesh, materials: [material])
+
+
             subject.addChild(meshComponent)
 
             content.add(subject)
 
+        } update: { content in
 
-        } update: { content, attachments in
-
-        } attachments: {
-            Attachment(id: "AttachmentContent") {
-                Text("")
-            }
         }
     }
-    func simplePath(in rect: CGRect) -> Path {
+    func simplePath() -> Path {
+        let rect = CGRect(x: -0.1, y: -0.1, width: 0.2, height: 0.2)
         var path = Path()
 
-        path.move(to: CGPoint(x: rect.midX, y: rect.maxY)) // tip at bottom
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY)) // left corner at top
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY)) // right corner at top
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY)) // close the path
+        path.move(to: CGPoint(x: rect.midX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
 
         return path
     }
 }
-
-
 
 #Preview {
     Lab059()
