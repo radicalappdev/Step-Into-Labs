47b21b7473a6a10d049da2b4d63fd861a65083bc
Joseph Simpson <joe@radicalappdev.com>
2025-06-17 15:05:06 -0400

    Display all transform data as the entity is moved

--- Step Into Labs/Labs/Lab063.swift
+++ Step Into Labs/Labs/Lab063.swift
@@ -39,12 +39,16 @@ struct Lab063: View {
         .ornament(attachmentAnchor: .scene(.back), ornament: {
             List {
                 Section("Observed Entity Data", content: {
-                    HStack {
-                        Text("X Position:")
-                        Spacer()
-                        Text("\(subject.observable.position.x)")
+
+                    VectorDisplay(title: "Position", vector: subject.observable.position)
+
+                    VStack(alignment: .leading) {
+                        Text("Rotation \(subject.observable.orientation.angle)")
+                            .fontWeight(.bold)
+                        VectorDisplay(title: "", vector: subject.observable.orientation.axis)
                     }
 
+                    VectorDisplay(title: "Scale", vector: subject.observable.scale)
                 })
             }
             .padding(.top, 12)
@@ -57,4 +61,23 @@ struct Lab063: View {
 
 #Preview("Volume", windowStyle: .volumetric) {
     Lab063()
+}
+
+
+fileprivate struct VectorDisplay: View {
+    let title: String
+    let vector: SIMD3<Float>
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .fontWeight(.bold)
+            HStack {
+                ForEach(["X", "Y", "Z"], id: \.self) { axis in
+                    Text("\(axis): \(String(format: "%8.3f", axis == "X" ? vector.x : axis == "Y" ? vector.y : vector.z))")
+                        .frame(width: 120, alignment: .leading)
+                }
+            }
+        }
+    }
 }
