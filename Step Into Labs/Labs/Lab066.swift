50a29ec9dd54cc825a23de6b35e30a5037b4cdc3
Joseph Simpson <joe@radicalappdev.com>
2025-07-13 10:42:34 -0400

    Starting work on lab 066

--- Step Into Labs/Labs/Lab066.swift
+++ Step Into Labs/Labs/Lab066.swift
@@ -15,15 +15,49 @@ import RealityKit
 import RealityKitContent
 
 struct Lab066: View {
+    @State var instanceCount = 3
+    @State var offset: Float = 0.05
+
     var body: some View {
-        RealityView { content, attachments in
+        RealityView { content in
 
-        } update: { content, attachments in
+            // Load a scene
+            guard let scene = try? await Entity(named: "InstanceLab", in: realityKitContentBundle) else { return }
+            content.add(scene)
 
-        } attachments: {
-            Attachment(id: "AttachmentContent") {
-                Text("")
+            // Load an entity
+            guard let subject = scene.findEntity(named: "Subject") else { return }
+
+            // Create and set up the component
+            var meshInstancesComponent = MeshInstancesComponent()
+            do {
+                let instances = try LowLevelInstanceData(instanceCount: 20)
+                // The tricky part can be getting the correct part index
+                meshInstancesComponent[partIndex: 0] = .init(data: instances)
+
+                // Loop over each instance and update the transform
+                instances.withMutableTransforms { transforms in
+                    for i in 0..<instanceCount {
+
+                        // For this example, we'll only edit the position / translation. We can also edit the scale and rotation if needed.
+                        let offset: Float = 0.05 * Float(i)
+                        var transform = Transform()
+
+                        transform.translation = [offset, offset, offset]
+                        transforms[i] = transform.matrix
+
+                    }
+                }
+
+                subject.components.set(meshInstancesComponent)
+            } catch {
+                print("error creating instances = \(error)")
             }
+            content.add(subject)
+            subject.position = .init(x: 0, y: -0.4, z: 0)
+
+
+
         }
     }
 }
--- Step Into Labs/Model/DirectoryModel.swift
+++ Step Into Labs/Model/DirectoryModel.swift
@@ -496,11 +496,11 @@ class ModelData {
              subtitle: "First look at the Manipulable modifier",
              description: "A SwiftUI modifier that works just like Manipulation Component from RealityKit.")
 
-        ,Lab(title: "Lab 065",
-             type: .SPACE,
-             date: Date("7/72025"),
+        ,Lab(title: "Lab 066",
+             type: .VOLUME,
+             date: Date("7/13/2025"),
              isFeatured: true,
-             subtitle: "TBD",
+             subtitle: "First look at MeshInstancesComponent",
              description: "")
 
 
