7c3e1abe3ec6155c65874f54fc08222f9c3fabc0
Joseph Simpson <joe@radicalappdev.com>
2025-08-26 07:36:49 -0400

    Starting lab 079

--- Step Into Labs/Labs/Lab079.swift
+++ Step Into Labs/Labs/Lab079.swift
@@ -19,6 +19,30 @@ struct Lab079: View {
     var body: some View {
         RealityView { content in
 
+            guard let scene = try? await Entity(named: "ManipulatePhysics", in: realityKitContentBundle) else { return }
+            content.add(scene)
+
+            scene.position.y = -0.4
+
+            if let subject1 = scene.findEntity(named: "Subject_1") {
+                // Using the ManipulationComponent defaults
+                let mc = ManipulationComponent()
+                subject1.components.set(mc)
+            }
+
+            if let subject2 = scene.findEntity(named: "Subject_2") {
+                // Using the ManipulationComponent defaults
+                let mc = ManipulationComponent()
+                subject2.components.set(mc)
+            }
+
+            if let subject3 = scene.findEntity(named: "Subject_3") {
+                // Using the ManipulationComponent defaults
+                let mc = ManipulationComponent()
+                subject3.components.set(mc)
+            }
+
+
 
         }
     }
