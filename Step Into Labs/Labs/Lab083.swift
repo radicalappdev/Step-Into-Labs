de76aeba53625a3d7f2284c0da78d63e2378425b
Joseph Simpson <joe@radicalappdev.com>
2025-09-18 06:12:55 -0400

    Basic animation

--- Step Into Labs/Labs/Lab083.swift
+++ Step Into Labs/Labs/Lab083.swift
@@ -15,14 +15,26 @@ import RealityKit
 import RealityKitContent
 
 struct Lab083: View {
+    @State private var progress: CGFloat = 0.0
+
     var body: some View {
         ZStack {
-
             Text("Feathered Glass Background Effect")
                 .font(.extraLargeTitle)
-            
         }
-        .glassBackgroundEffect(.feathered(padding: 36, softEdgeRadius: 3), displayMode: .always)
+        .glassBackgroundEffect(
+            .feathered(
+                padding: 36,
+                softEdgeRadius: 3 + (9 * progress) // min 3, animates up to 12
+            ),
+            displayMode: .always
+        )
+
+        .onAppear {
+            withAnimation(.easeInOut(duration: 4.0).repeatForever()) {
+                progress = 1.0
+            }
+        }
     }
 }
 
