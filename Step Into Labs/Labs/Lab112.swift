8d0783f25d2dd9ceeb6d26938c63baa213d87441
Joseph Simpson <joe@radicalappdev.com>
2026-05-20 15:35:40 -0400

    Starting Lab 112

---
+++ Step Into Labs/Labs/Lab112.swift
@@ -0,0 +1,34 @@
+//  Step Into Vision - Labs
+//
+//  Title: Lab112
+//
+//  Subtitle: Arc Slider
+//
+//  Description:
+//
+//  Type: Volume
+//
+//  Featured:
+//
+//  Created by Joseph Simpson on 5/20/26.
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct Lab112: View {
+    var body: some View {
+        VStack {
+
+            RoundedRectangle(cornerRadius: 12)
+                .fill(.stepRed)
+                .frame(width: 200, height: 160)
+                
+
+        }
+    }
+}
+
+#Preview {
+    Lab112()
+}
